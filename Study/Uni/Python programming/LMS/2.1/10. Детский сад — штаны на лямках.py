6d158f5a6d664a2a006bf18927449c1bb00dba19
MrBebra <issuslov@gmail.com>
2024-10-26 10:20:17 +0300

    update

--- Study/Uni/Python programming/LMS/2.1/10. Детский сад — штаны на лямках.py
+++ Study/Uni/Python programming/LMS/2.1/10. Детский сад — штаны на лямках.py
@@ -1,11 +1,14 @@
+#запрашиваем данные у пользователя
 name = input()
 id = input()
 
+#Разделяем строку на части
 group = id[0]
 bed = id[1]
 locker = id
 number = id[2]
 
+#Выводим сформированную карточку в консоль
 print(f'Группа №{group}.')
 print(f'{number}. {name}.')
 print(f'Шкафчик: {locker}.')
--- Study/Uni/Python programming/LMS/2.1/6. Чек.py
+++ Study/Uni/Python programming/LMS/2.1/6. Чек.py
@@ -1,14 +1,14 @@
-# Input from the user
+#Получаем данные от пользователя
 product_name = input("Введите название товара: ")
 price_per_kg = float(input("Введите цену товара (руб/кг): "))
 weight = float(input("Введите вес товара (кг): "))
 money_given = float(input("Введите количество денег у пользователя (руб): "))
 
-# Calculating total cost and change
+#Вычисляем общую стоимость и сдачу
 total_cost = price_per_kg * weight
 change = money_given - total_cost
 
-# Printing the receipt
+#Печатем чек
 print("Чек")
 print(f"{product_name} - {weight}кг - {price_per_kg}руб/кг")
 print(f"Итого: {total_cost}руб")
